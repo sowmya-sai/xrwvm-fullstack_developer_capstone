51938d897c23a11c35719bb8e3e374c1c69afaf1
sowmya-sai <sowmisomu11@gmail.com>
2024-08-04 09:12:02 -0400

    changes made from theia lab environment

--- server/database/app.js
+++ server/database/app.js
@@ -57,20 +57,36 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 });
 
 // Express route to fetch all dealerships
-app.get('/fetchDealers', async (req, res) => {
-//Write your code here
-});
 
+app.get('/fetchDealers', async (req, res) => {
+    try {
+      const documents = await Dealerships.find();
+      res.json(documents);
+    } catch (error) {
+      res.status(500).json({ error: 'Error fetching documents' });
+    }
+});
 // Express route to fetch Dealers by a particular state
+
 app.get('/fetchDealers/:state', async (req, res) => {
-//Write your code here
+    try {
+      const documents = await Dealerships.find({ state: req.params.state });
+      res.json(documents);
+    } catch (error) {
+      res.status(500).json({ error: 'Error fetching documents' });
+    }
 });
 
 // Express route to fetch dealer by a particular id
-app.get('/fetchDealer/:id', async (req, res) => {
-//Write your code here
-});
 
+app.get('/fetchDealer/:id', async (req, res) => {
+    try {
+      const document = await Dealerships.findById(req.params.id);
+      res.json(document);
+    } catch (error) {
+      res.status(500).json({ error: 'Error fetching document' });
+    }
+});
 //Express route to insert review
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
   data = JSON.parse(req.body);
