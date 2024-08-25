f2ee855cf7c983e5551d54303b85415aeabe7eab
sowmya-sai <sowmisomu11@gmail.com>
2024-08-25 03:56:18 -0400

    changes made from theia lab environment

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -11,15 +11,9 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY =\
     'django-insecure-ccow$tz_=9%dxu4(0%^(z%nx32#s@(zt9$ih@)5l54yny)wm-0'
-DEBUG = True if some_condition == True else False
-ALLOWED_HOSTS = ['localhost',
-url= "https://sowmisomu11-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01"
-    ".proxy.cognitiveclass.ai"
-]
-CSRF_TRUSTED_ORIGINS = [
-url="https://sowmisomu11-8000.theiadockernext-"
-    "1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"
-]
+DEBUG = True
+ALLOWED_HOSTS = ['localhost','https://sowmisomu11-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS = ['https://sowmisomu11-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
 }
